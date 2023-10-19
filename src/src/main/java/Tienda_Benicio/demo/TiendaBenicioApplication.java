c45073393d64ac84b0875733e89bdc3244ca228d
BCA1504 <bcence50485@ufide.ac.cr>
2023-10-18 18:19:57 -0600

    Cambios semana 6 (antes de clase)

---
+++ src/src/main/java/Tienda_Benicio/demo/TiendaBenicioApplication.java
@@ -0,0 +1,13 @@
+package Tienda_Benicio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaBenicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaBenicioApplication.class, args);
+	}
+
+}
---
+++ src/src/test/java/Tienda_Benicio/demo/TiendaBenicioApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda_Benicio.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaBenicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
